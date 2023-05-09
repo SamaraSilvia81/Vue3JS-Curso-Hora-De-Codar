f57f9a3766cfdd047b8ce4a781af183f709a16dc
Samara Silvia <samara.silvia@bsit.intra>
2023-05-09 09:20:50 -0300

    Added LyfeCycle

--- 2_project_vue/src/App.vue
+++ 2_project_vue/src/App.vue
@@ -1,22 +1,24 @@
 <template>
   <div id="app">
-    <!--<h1>Hello World</h1>-->
-    <!--Essa chamada funciona com letras minusculas e um (-) separando tbm-->
     <FirstComponent/>
-    Test: {{test }}
+    <p>The data app is {{ test }}</p>
+    <LyfeCycle/>
   </div>
 </template>
 
 <script>
+
   import FirstComponent from './components/FirstComponent.vue';
+  import LyfeCycle from './components/LyfeCycle.vue';
 
   export default {
-    // Assim o vue irá saber que esse template é a minha página app
     name: 'App',
-    // Criando um outro objeto declarando ao vue que usaremos components
+
     components: {
-      FirstComponent
+      FirstComponent,
+      LyfeCycle
     },
+
     data(){
       return {
         test: "Testing"
---
+++ 2_project_vue/src/components/LyfeCycle.vue
@@ -0,0 +1,30 @@
+<template>
+    <h1>My name is: {{ name }}</h1>
+</template>
+
+<script>
+export default {
+    name: 'LyfeCycle',
+    data() {
+        return {
+            name: "I don't know yet"
+        }
+    },
+    // Hook que vai refletir na minha aplicação
+    // Me ajudará a tornar dinâmico os meus dados
+    created(){
+        // this.name = "Samara"
+        // Ao fazer isso passamos por cima do "mounted"
+        setTimeout(() => {
+            this.name = "Samara"
+        }, 1000)
+    },
+    // Hook que está acima da hierarquia
+    mounted(){
+        // Isso nos ajuda a vê melhor o que está acontecendo
+        setTimeout(() => {
+            this.name = "Matheus"
+        }, 2000)
+    }
+}
+</script>
